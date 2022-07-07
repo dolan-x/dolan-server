79b21aebb2320cd1c4fa3b8ac0e129a86360d613
so1ve <nn_201312@163.com>
2022-07-08 01:14:53 +0800

    fix

--- src/lib/jwt_key.ts
+++ src/lib/jwt_key.ts
@@ -3,15 +3,3 @@ export const jwtKey = await crypto.subtle.generateKey(
   true,
   ["sign", "verify"],
 );
-
-const key = await crypto.subtle.generateKey(
-  { name: "HMAC", hash: "SHA-512" },
-  true,
-  ["sign", "verify"],
-);
-
-const exportedKey = await crypto.subtle.exportKey("jwk", key);
-Deno.writeFileSync(
-  "./key.json",
-  new TextEncoder().encode(JSON.stringify(exportedKey)),
-);
