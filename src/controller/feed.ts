792248c4c9bafe7a8b68352d7f5a3239b5bcea3d
so1ve <nn_201312@163.com>
2023-01-10 19:16:20 +0800

    feat: add categories to feed

--- src/controller/feed.ts
+++ src/controller/feed.ts
@@ -1,4 +1,4 @@
-import { ConfigSite, Post } from "@dolan-x/shared";
+import { Category, ConfigSite, Post } from "@dolan-x/shared";
 import { renderMarkdownSync } from "@dolan-x/markdown-renderer";
 import { Feed } from "feed";
 import * as log from "$log";
@@ -8,19 +8,24 @@ import { getStorage } from "../lib/mod.ts";
 import { getConfig } from "../utils/mod.ts";
 
 const postsStorage = getStorage("Posts");
+const categoriesStorage = getStorage("Categories");
 
 /**
  * GET /feed
  */
 export const generateFeed: RouterMiddleware<"/feed"> = async (ctx) => {
   log.info("Feed: Generating feed");
-  const [posts, config] = await Promise.all([
+  const [posts, categories, config] = await Promise.all([
     postsStorage.select(
       { status: ["!=", "draft"] },
       {
         fields: ["title", "slug", "excerpt", "content", "updated"],
       },
     ) as Promise<Post[]>,
+    categoriesStorage.select(
+      {},
+      { fields: ["name"] },
+    ) as Promise<Category[]>,
     getConfig("site") as Promise<ConfigSite>,
   ]);
   const { url, name, description, postsBaseUrl } = config;
@@ -31,6 +36,9 @@ export const generateFeed: RouterMiddleware<"/feed"> = async (ctx) => {
     link: url,
     copyright: "", // TODO(@so1ve)
     generator: "Dolan",
+  });
+  categories.forEach(({ name }) => {
+    feed.addCategory(name);
   });
   posts.forEach(({ title, slug, excerpt, content, updated }) => {
     console.log(updated);
